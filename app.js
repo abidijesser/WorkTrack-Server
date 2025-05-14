e20057da925572533bd1439c96b2890f928f4399
abidijesser <jasserabidi00@gmail.com>
2025-05-14 18:39:00 +0200

    added CORS to support github pages

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "http://127.0.0.1:3000",
+      "https://yourusername.github.io",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
