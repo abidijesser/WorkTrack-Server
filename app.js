8ddb29a711bf7ad91125ff5f6ef58e0ff51eb54f
abidijesser <jasserabidi00@gmail.com>
2025-05-14 18:45:49 +0200

    corrected URL github page

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "http://127.0.0.1:3000",
-      "https://yourusername.github.io",
+      "https://abidijesser.github.io/WorkTrack",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
